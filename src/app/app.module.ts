5f2a9f9d0a76104e19daa7171770a5f69c48a5b9
Andreas Stutz <andreas.stutz@siemens.com>
2021-04-17 02:33:31 +0200

    Basic Layout for SCORE App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {CardModule} from 'primeng/card';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './shell/app-header/app-header.component';
+import { AppMainComponent } from './shell/app-main/app-main.component';
+import { AppFooterComponent } from './shell/app-footer/app-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppMainComponent,
+    AppFooterComponent
   ],
   imports: [
     BrowserModule,
+    CardModule,
+    ToolbarModule,
+    ButtonModule,
     AppRoutingModule
   ],
   providers: [],
