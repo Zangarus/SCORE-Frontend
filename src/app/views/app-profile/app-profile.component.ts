24553de5312b42f19ab1c4de5930bff2d9f8c376
unknown <rohlins@inbox.lv>
2021-04-17 17:36:36 +0200

    :q!
    gi

--- src/app/views/app-profile/app-profile.component.ts
+++ src/app/views/app-profile/app-profile.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-app-profile',
   templateUrl: './app-profile.component.html',
-  styleUrls: ['./app-profile.component.css']
+  styleUrls: ['./app-profile.component.css'],
+  providers: [MessageService]
 })
 export class AppProfileComponent implements OnInit {
 
-  constructor() { }
+  value: number = 0;
 
-  ngOnInit(): void {
+  constructor(private messageService: MessageService) {}
+
+  ngOnInit() {
+    let interval = setInterval(() => {
+      this.value = 77;
+      if (this.value >= 100) {
+        this.value = 100;
+        this.messageService.add({severity: 'info', summary: 'Success', detail: 'Process Completed'});
+        clearInterval(interval);
+      }
+    }, 2000);
   }
 
 }
+
