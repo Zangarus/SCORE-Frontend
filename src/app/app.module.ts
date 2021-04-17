24ea274910c590ad94440faa45c5a1f5f77297df
Michelle Blumenstein <michelle.blumenstein@hsu-hh.de>
2021-04-17 17:21:39 +0200

    added home view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {CardModule} from 'primeng/card';
 import {ToolbarModule} from 'primeng/toolbar';
 import {ButtonModule} from 'primeng/button';
 import {MenuModule} from 'primeng/menu';
+import {FormsModule} from '@angular/forms';
+import {DropdownModule} from 'primeng/dropdown';
+import {KnobModule} from 'primeng/knob';
+import {ProgressBarModule} from 'primeng/progressbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,11 +40,16 @@ import { AppStatisticsComponent } from './views/app-statistics/app-statistics.co
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CardModule,
     ToolbarModule,
     ButtonModule,
     AppRoutingModule,
-    MenuModule
+    MenuModule,
+    FormsModule,
+    DropdownModule,
+    KnobModule,
+    ProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/app-home/app-home.component.ts
+++ src/app/views/app-home/app-home.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Vehicle {
+  name: string,
+  code: string
+}
+
 @Component({
   selector: 'app-app-home',
   templateUrl: './app-home.component.html',
   styleUrls: ['./app-home.component.css']
 })
+
 export class AppHomeComponent implements OnInit {
 
-  constructor() { }
+  score : number = 40
+  valueChallenge : number = 10
+  valueChallengeFriend : number =30
+
+  vehicles : Vehicle[];
+  selectedVehicle : string ='';
+
+  constructor() {
+
+    this.vehicles = [
+      {name: 'Auto', code: 'aut'},
+      {name: 'Fahrrad', code: 'fahr'}
+  ];
+
+
+  }
 
   ngOnInit(): void {
+
   }
 
 }
