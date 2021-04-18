ae449a45fa90b2d9a22858224712c056aee5841d
unknown <rohlins@inbox.lv>
2021-04-18 02:07:05 +0200

    Commit after Andreas (around 2AM)

--- src/app/views/app-profile/app-profile.component.ts
+++ src/app/views/app-profile/app-profile.component.ts
@@ -15,14 +15,13 @@ export class AppProfileComponent implements OnInit {
   constructor(private messageService: MessageService) {}
 
   ngOnInit() {
-    let interval = setInterval(() => {
+
       this.value = 77;
       if (this.value >= 100) {
         this.value = 100;
         this.messageService.add({severity: 'info', summary: 'Success', detail: 'Process Completed'});
-        clearInterval(interval);
       }
-    }, 2000);
+
   }
 
 }
